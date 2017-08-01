afdee32484e9830b1685e558ff5000c257d0b9d8
Antoine Lochet <invik83+github_al@gmail.com>
2017-08-01 15:42:06 +0200

    Removed problematic scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -220,9 +220,9 @@ jQuery(function ($) {
     // -------------------------------------------------------------
     // Vidio auto play
     // -------------------------------------------------------------
-    (function () {
+    /*(function () {
     
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
+    *//* Vimeo API: http://developer.vimeo.com/player/js-api *//*
     
         var iframe = document.getElementById('nofocusvideo');
         // $f == Froogaloop
@@ -235,7 +235,7 @@ jQuery(function ($) {
         $('.modal').on('shown.bs.modal', function () {
         player.api('play');
         })
-    }());
+    }());*/
 
 
 
